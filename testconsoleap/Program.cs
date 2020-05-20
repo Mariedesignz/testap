01470a4b818718bc5258cfa50432e73bf44fc68e
Marie Designz (Tina Marie) <55719730+Mariedesignz@users.noreply.github.com>
2020-05-19 19:38:18 -0700

    Init commit

---
+++ testconsoleap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testconsoleap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
